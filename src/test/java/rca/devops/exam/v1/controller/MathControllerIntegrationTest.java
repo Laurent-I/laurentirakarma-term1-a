8849410888d34264ac9ceed1cec6771091c2accd
Laurent I <laurent@Laurent.law>
2023-12-18 10:44:17 +0200

    fix: Used Mockito and JUnit for testing

--- src/test/java/rca/devops/exam/v1/controller/MathControllerIntegrationTest.java
+++ src/test/java/rca/devops/exam/v1/controller/MathControllerIntegrationTest.java
@@ -1,26 +1,42 @@
 package rca.devops.exam.v1.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import rca.devops.exam.v1.exception.InvalidOperationException;
+import rca.devops.exam.v1.service.MathOperator;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class MathControllerIntegrationTest {
 
-    @Autowired
+    @Mock
+    private MathOperator mathOperator;
+
+    @InjectMocks
+    private MathController mathController;
+
     private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(mathController).build();
+    }
 
     @Test
     public void testDoMathEndpointAddition() throws Exception {
+        when(mathOperator.doMath(5, 3, "+")).thenReturn(8.0);
+
         mockMvc.perform(post("/doMath")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"operand1\":5, \"operand2\":3, \"operation\":\"+\"}"))
@@ -29,6 +45,8 @@ public class MathControllerIntegrationTest {
 
     @Test
     public void testDoMathEndpointSubtraction() throws Exception {
+        when(mathOperator.doMath(5, 3, "-")).thenReturn(2.0);
+
         mockMvc.perform(post("/doMath")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"operand1\":5, \"operand2\":3, \"operation\":\"-\"}"))
@@ -37,6 +55,8 @@ public class MathControllerIntegrationTest {
 
     @Test
     public void testDoMathEndpointMultiplication() throws Exception {
+        when(mathOperator.doMath(5, 3, "*")).thenReturn(15.0);
+
         mockMvc.perform(post("/doMath")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"operand1\":5, \"operand2\":3, \"operation\":\"*\"}"))
@@ -45,15 +65,18 @@ public class MathControllerIntegrationTest {
 
     @Test
     public void testDoMathEndpointDivision() throws Exception {
+        when(mathOperator.doMath(6, 3, "/")).thenReturn(2.0);
+
         mockMvc.perform(post("/doMath")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"operand1\":6, \"operand2\":3, \"operation\":\"/\"}"))
                 .andExpect(status().isOk());
     }
 
-
     @Test
     public void testDoMathEndpointDivisionByZero() throws Exception {
+        when(mathOperator.doMath(5, 0, "/")).thenThrow(new InvalidOperationException("Cannot divide by 0"));
+
         mockMvc.perform(post("/doMath")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"operand1\":5, \"operand2\":0, \"operation\":\"/\"}"))
@@ -63,6 +86,8 @@ public class MathControllerIntegrationTest {
 
     @Test
     public void testDoMathEndpointUnknownOperation() throws Exception {
+        when(mathOperator.doMath(5, 3, "^")).thenThrow(new RuntimeException("Unknown operation"));
+
         mockMvc.perform(post("/doMath")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"operand1\":5, \"operand2\":3, \"operation\":\"^\"}"))
--- src/test/java/rca/devops/exam/v1/service/MathOperatorImplTest.java
+++ src/test/java/rca/devops/exam/v1/service/MathOperatorImplTest.java
@@ -1,14 +1,25 @@
 package rca.devops.exam.v1.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import rca.devops.exam.v1.exception.InvalidOperationException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ExtendWith(MockitoExtension.class)
 public class MathOperatorImplTest {
 
-    private final MathOperator mathOperator = new MathOperatorImpl();
+    @InjectMocks
+    private MathOperatorImpl mathOperator;
+
+    @BeforeEach
+    public void setup() {
+        mathOperator = new MathOperatorImpl();
+    }
 
     @Test
     public void testDoMathAddition() throws InvalidOperationException {
